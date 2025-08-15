027b40707b3f58bcae1bcc27166d52b88e341389
Colm Talbot <talbotcolm@gmail.com>
2025-08-15 14:46:49 -0500

    FMT: format toy model

--- demos/toy_model.py
+++ demos/toy_model.py
@@ -9,8 +9,8 @@ def func(data, x, y):
 
 def setup_toy_likelihood_and_priors():
     priors = bilby.core.prior.PriorDict()
-    priors['x'] = bilby.core.prior.Uniform(-1, 2, 'x')
-    priors['y'] = bilby.core.prior.Uniform(-1, 2, 'y')
+    priors["x"] = bilby.core.prior.Uniform(-1, 2, "x")
+    priors["y"] = bilby.core.prior.Uniform(-1, 2, "y")
 
     data = np.random.normal(0, 1, (1000, 2))
     points = np.linspace(0, 1, 1000)
@@ -26,18 +26,19 @@ def setup_toy_likelihood_and_priors():
 
 
 if __name__ == "__main__":
-
     likelihood, priors = setup_toy_likelihood_and_priors()
     results = list()
     for nsteps in [3, 10, 30, 100]:
-        results.append(bilby.run_sampler(
-            likelihood,
-            priors,
-            sampler="jaxted",
-            outdir="test",
-            label="toy",
-            injection_parameters={'x': 0.7, 'y': 0.7},
-            nsteps=nsteps,
-            save="hdf5",
-        ))
+        results.append(
+            bilby.run_sampler(
+                likelihood,
+                priors,
+                sampler="jaxted",
+                outdir="test",
+                label="toy",
+                injection_parameters={"x": 0.7, "y": 0.7},
+                nsteps=nsteps,
+                save="hdf5",
+            )
+        )
     bilby.core.result.plot_multiple(results, filename="test/toy_model.png")
